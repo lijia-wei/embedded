5cab62b591c4a2445855f1ac9dbeaee805bac444
lijia-wei <1436464371@qq.com>
2020-06-04 19:43:49 +0800

    mytest.txt

---
+++ 初见&甜品屋/js/index.js
@@ -0,0 +1,84 @@
+$(function(){
+	//导航
+	$('.h-con li').hover(function(){
+		$(this).toggleClass('current');
+	})
+	//关闭广告
+	$('.h-close').click(function(){
+		$('.h-new').hide();
+	})
+	//下拉栏
+	$('.h-con .more').hover(function(){
+		$(this).children('.more-con').stop().slideToggle('fast');
+	})
+	//轮播图
+	var index=0;
+	var f;
+	$('.box-img').eq(index).fadeIn(0).siblings('.box-img').fadeOut(0);
+	function dinshiqi(){
+		f = setInterval(function(){
+		$('.box-img').eq(index).fadeIn().siblings('.box-img').fadeOut();
+		$('.botton').eq(index).css('background-color', '#fff').siblings('.botton').css('background-color', '#ccc');
+		if(index==$('.box-img').length-1){
+			index=-1;
+		}
+		index++;
+		}, 4000)
+	}
+	dinshiqi();
+	$('.box-left').on('click', function(){
+		clearInterval(f);
+		if(index==0){
+			index=$('.box-img').length
+		}
+		index--;
+		$('.box-img').eq(index).fadeIn().siblings('.box-img').fadeOut();
+		$('.botton').eq(index).css('background-color', '#fff').siblings('.botton').css('background-color', '#ccc');
+		dinshiqi();
+	})
+	$('.box-right').on('click', function(){
+		clearInterval(f);
+		if(index==$('.box-img').length-1){
+			index=-1;
+		}
+		index++;
+		$('.box-img').eq(index).fadeIn().siblings('.box-img').fadeOut();
+		$('.botton').eq(index).css('background-color', '#fff').siblings('.botton').css('background-color', '#ccc');
+		dinshiqi();
+	})
+	$('.botton').on('click', function(){
+		clearInterval(f);
+		var indexx=$(this).index();
+		index=indexx;
+		$('.box-img').eq(index).fadeIn().siblings('.box-img').fadeOut();
+		$('.botton').eq(index).css('background-color', '#fff').siblings('.botton').css('background-color', '#ccc');
+		dinshiqi();
+	})
+	// 切换栏
+	$('.musi ul>li').hover(function(){
+		var index=0;
+		index=$(this).index();
+		$('.tab-con img').eq(index).stop().css('opacity', '1').siblings().css('opacity', '0');
+	})
+	//滑动遮挡效果
+	function slide(){
+		for(var index=0, l=22; index<6; index++){
+			$('.mkl ul img').eq(index).css('right', l+'%');
+			l+=10;
+		}
+	}
+	slide();
+	$('.mkl ul img').hover(function(){
+		var index=$(this).index();
+		$(this).eq(index).toggleClass('mkl_cur');
+	})
+	// 侧边栏
+	$(window).scroll(function(){
+		if($(window).scrollTop()>=700){
+			$('.slider-bar').fadeIn();
+		}
+		else{
+			$('.slider-bar').fadeOut();
+		}
+	})
+})
---
+++ 初见&甜品屋/js/shopCar.js
@@ -0,0 +1,110 @@
+$(function(){
+	//全选
+	$('.checkall').change(function(){
+		$('.single, .checkall').prop('checked', $(this).prop('checked'));
+		if($(this).prop('checked')){
+			$('.c-good ul').css('background-color', '#EEDC82');
+		}
+		else{
+			$('.c-good ul').css('background-color', '#EEDFCC');
+		}
+		getSum();
+	});
+	$('.single').change(function(){
+		if($('.single:checked').length===$('.single').length){
+			$(".checkall").prop('checked', true);
+		}
+		else{
+			$(".checkall").prop('checked', false);
+		}
+		if($(this).prop("checked")){
+			$(this).parents('ul').css('background-color', '#EEDC82');
+		}else{
+			$(this).parents('ul').css('background-color', '#EEDFCC');
+		}
+		getSum();
+	});
+	$('.single').trigger('change');
+	//小计函数
+	function xiaoji(n, target){
+		var s=$(target).parent().siblings('.s-price').html();
+		s=s.substr(1);
+		$(target).parent().siblings('.s-num').html('￥'+(s*n).toFixed(2));
+	}
+	getSum();
+	//求和函数
+	function getSum(){
+		var count = 0;
+		var money = 0;		
+		$('.itxt').each(function(i, ele){
+			if($(ele).parent().siblings('.choose').children().prop('checked')){
+				count+=parseFloat($(ele).val());
+			}		
+		});
+		$('.bought em').text(count);
+		$('.s-num').each(function(i, ele){
+			if($(ele).siblings('.choose').children().prop('checked')){
+				money += parseFloat($(ele).text().substr(1));
+			}	
+		});
+		$('.allnum em').text("￥"+money.toFixed(2));
+	}
+	function empty(){
+		setTimeout(function(){
+			if($('.single').length==0){
+			alert('购物车为空快去选购吧！');
+			}
+		}, 1);
+	}
+	//数量
+	$('.add').click(function(){
+		var n=$(this).siblings('.itxt').val();
+		n++;
+		$(this).siblings('.itxt').val(n);
+		//小计
+		xiaoji(n, this);
+		getSum();
+	});
+	$('.sub').click(function(){
+		var n=$(this).siblings('.itxt').val();
+		if(n==1){
+			return false;
+		}
+		n--;
+		$(this).siblings('.itxt').val(n);
+		//小计
+		xiaoji(n, this);
+		getSum();
+	});
+	//需修改文本框
+	$('.itxt').change(function(){
+		var n=$(this).val();
+		xiaoji(n, this);
+		getSum();
+	});
+	$('.itxt').trigger('change');
+	//删除
+	$('.del a').click(function(){
+		$(this).parents('ul').remove();
+		getSum();
+		empty();
+	});
+	$('.delchecked').click(function(){
+		$('.single:checked').parents('ul').remove();
+		getSum();
+		empty();
+	});
+	$('.delall').click(function(){
+		$(".c-good ul").remove();
+		getSum();
+		empty();
+	});
+	$(window).scroll(function(){
+		if($(window).scrollTop()>=400){
+			$('.slider-bar').fadeIn();
+		}
+		else{
+			$('.slider-bar').fadeOut();
+		}
+	})
+})
